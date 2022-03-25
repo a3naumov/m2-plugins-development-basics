cc41bc2e3a8ac62a28146d09b6b92c9865eb31d4
a3.naumov <a3.naumov@itransition.com>
2022-03-25 12:36:51 +0300

    Add Data Provider for extensions grid

---
+++ Learning/ExtensionAttribute/Ui/DataProvider/Attributes/ListingDataProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Learning\ExtensionAttribute\Ui\DataProvider\Attributes;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider;
+
+class ListingDataProvider extends DataProvider
+{
+
+}
