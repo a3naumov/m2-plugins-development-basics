bcf6c90b4ff9bf4fbe347fabad98b39f29f848df
a3.naumov <a3.naumov@itransition.com>
2022-04-01 16:29:16 +0300

    Update js file for cartbuilder module

--- Learning/BuildCart/view/frontend/web/js/uploadForm.js
+++ Learning/BuildCart/view/frontend/web/js/uploadForm.js
@@ -1,44 +1,50 @@
-let inputField = document.getElementById('file');
-let input = document.querySelector('#file');
-let dropArea = document.querySelector('#drop-area');
-let dropAreaText = document.querySelector('#drop-area-text');
+define([
+], function () {
+    "use strict";
+    return function (config, element) {
+        let inputField = document.getElementById('file');
+        let input = document.querySelector('#file');
+        let dropArea = document.querySelector('#drop-area');
+        let dropAreaText = document.querySelector('#drop-area-text');
 
-inputField.addEventListener('change', function () {
-    if(this.value) {
-        if (this.files[0].type !== 'text/csv') {
-            dropAreaText.textContent = 'Please upload csv file!';
-            dropAreaText.classList.add('danger-error');
-            dropArea.classList.add('active-area');
-            dropArea.classList.remove('with-file');
-            input.value = '';
-            input.removeAttribute('hidden');
-        } else {
-            dropArea.classList.remove('active-area');
-            dropArea.classList.add('with-file');
-            dropAreaText.classList.remove('danger-error');
-            dropAreaText.textContent = this.files[0].name;
-            dropAreaText.style.marginBottom = '20px';
+        inputField.addEventListener('change', function () {
+            if(this.value) {
+                if (this.files[0].type !== 'text/csv') {
+                    dropAreaText.textContent = 'Please upload csv file!';
+                    dropAreaText.classList.add('danger-error');
+                    dropArea.classList.add('active-area');
+                    dropArea.classList.remove('with-file');
+                    input.value = '';
+                    input.removeAttribute('hidden');
+                } else {
+                    dropArea.classList.remove('active-area');
+                    dropArea.classList.add('with-file');
+                    dropAreaText.classList.remove('danger-error');
+                    dropAreaText.textContent = this.files[0].name;
+                    dropAreaText.style.marginBottom = '20px';
 
-            input.style.cursor = 'auto';
-            input.setAttribute('hidden', '');
-            dropArea.insertAdjacentHTML('afterend',
-                '<button class="submit_form_btn" type="submit" form="form">Send</button>');
+                    input.style.cursor = 'auto';
+                    input.setAttribute('hidden', '');
+                    dropArea.insertAdjacentHTML('afterend',
+                        '<button class="submit_form_btn" type="submit" form="form">Send</button>');
+                }
+            }
+        });
+
+
+        inputField.addEventListener('dragenter', setActiveArea);
+        inputField.addEventListener('mouseover', setActiveArea);
+        inputField.addEventListener('dragleave', removeActiveArea);
+        inputField.addEventListener('mouseleave', removeActiveArea);
+
+        function setActiveArea() {
+            if (!inputField.value) {
+                document.querySelector('#drop-area').classList.add('active-area');
+            }
+        }
+
+        function removeActiveArea() {
+            document.querySelector('#drop-area').classList.remove('active-area');
         }
     }
 });
-
-
-inputField.addEventListener('dragenter', setActiveArea);
-inputField.addEventListener('mouseover', setActiveArea);
-inputField.addEventListener('dragleave', removeActiveArea);
-inputField.addEventListener('mouseleave', removeActiveArea);
-
-function setActiveArea() {
-    if (!inputField.value) {
-        document.querySelector('#drop-area').classList.add('active-area');
-    }
-}
-
-function removeActiveArea() {
-    document.querySelector('#drop-area').classList.remove('active-area');
-}
